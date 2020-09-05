b5465f7c05ab53fa976ceac229deceb1b82c82ac
Tuan Truong <tuan188@gmail.com>
2020-09-05 15:31:25 +0700

    Add Swift Package support

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Dto",
+    platforms: [
+        .iOS(.v9)
+    ],
+    products: [
+        .library(
+            name: "Dto",
+            targets: ["Dto"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/SvenTiigi/ValidatedPropertyKit.git", from: "0.0.4")
+    ],
+    targets: [
+        .target(
+            name: "Dto",
+            dependencies: [
+                .product(name: "ValidatedPropertyKit", package: "ValidatedPropertyKit"),
+            ],
+            path: "Dto/Sources"
+        ),    
+    ],
+    swiftLanguageVersions: [.v5]
+)
